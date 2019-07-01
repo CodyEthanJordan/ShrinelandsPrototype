d336d6f2e10a662807c90c3f810e5e2e3c914742
Cody Jordan <codyethanjordan@knights.ucf.edu>
2019-07-01 15:55:47 -0400

    adding project files

---
+++ Assets/Scripts/CombatManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ShrinelandsTactics;
+using UnityEngine.Tilemaps;
+
+public class CombatManager : MonoBehaviour
+{
+    public DungeonMaster DM;
+    public Tilemap tileMap;
+    public Tile basicTile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        DM = new DungeonMaster();
+
+        tileMap.ClearAllTiles();
+        tileMap.SetTile(Vector3Int.zero, basicTile);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
