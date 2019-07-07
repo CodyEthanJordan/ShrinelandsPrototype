da98db65191873c2b25e034c129767a92dbad981
CodyEthanJordan <codyethanjordan@users.noreply.github.com>
2019-07-06 20:55:24 -0400

    Drawing units

---
+++ Assets/Scripts/CharacterRenderer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using ShrinelandsTactics;
+using ShrinelandsTactics.World;
+
+namespace Assets.Scripts
+{
+    public class CharacterRenderer : MonoBehaviour
+    {
+        public Sprite knightSprite;
+        public Sprite slimeSprite;
+        public Sprite slimelordSprite;
+
+        public Character CharacterRepresented;
+
+        private SpriteRenderer sr;
+
+        private void Awake()
+        {
+            sr = GetComponent<SpriteRenderer>();    
+        }
+
+        public void RepresentCharacter(Character c)
+        {
+            this.CharacterRepresented = c;
+            switch(c.Class)
+            {
+                case "Knight":
+                    sr.sprite = knightSprite;
+                    break;
+                case "Slime":
+                    sr.sprite = slimeSprite;
+                    break;
+                case "Slimelord":
+                    sr.sprite = slimelordSprite;
+                    break;
+
+                default:
+                    throw new NotImplementedException();
+            }
+
+        }
+    }
+}
--- Assets/Scripts/CombatManager.cs
+++ Assets/Scripts/CombatManager.cs
@@ -7,83 +7,94 @@ using YamlDotNet.RepresentationModel;
 using System.IO;
 using System.Text;
 
-public class CombatManager : MonoBehaviour
+namespace Assets.Scripts
 {
-    public DungeonMaster DM;
-    public Tilemap tileMap;
-    public Tile emptyTile;
-    public Tile wallTile;
-
-    public TextAsset characterJson;
-    public TextAsset tileJson;
-    public TextAsset actionJson;
-    public TextAsset levelYaml;
-    public Texture2D levelLayout;
-
-    // Start is called before the first frame update
-    void Start()
+    public class CombatManager : MonoBehaviour
     {
-        var data = GameData.CreateFromJson(tileJson.text, characterJson.text, actionJson.text);
+        public DungeonMaster DM;
+        public Tilemap tileMap;
+        public Tile emptyTile;
+        public Tile wallTile;
+        public GameObject characterPrefab;
 
-        var yaml = new YamlStream();
-        using (StringReader r = new StringReader(levelYaml.text))
+        public TextAsset characterJson;
+        public TextAsset tileJson;
+        public TextAsset actionJson;
+        public TextAsset levelYaml;
+        public Texture2D levelLayout;
+
+        // Start is called before the first frame update
+        void Start()
         {
-            yaml.Load(r);
-        }
+            var data = GameData.CreateFromJson(tileJson.text, characterJson.text, actionJson.text);
 
-        var mapping = (YamlMappingNode)yaml.Documents[0].RootNode;
-        var mapFile = (mapping[new YamlScalarNode("map_file")] as YamlScalarNode).Value;
-
-
-        StringBuilder sb = new StringBuilder();
-        sb.AppendLine("Test Map");
-        sb.AppendLine(levelLayout.width + " " + levelLayout.height);
-
-        for (int y = 0; y < levelLayout.height; y++)
-        {
-            for (int x = 0; x < levelLayout.width; x++)
+            var yaml = new YamlStream();
+            using (StringReader r = new StringReader(levelYaml.text))
             {
-                var color = levelLayout.GetPixel(x, y);
-                var r = (byte)(color.r * 255);
-                var g = (byte)(color.g * 255);
-                var b = (byte)(color.b * 255);
-                sb.Append(data.GetIconByColor(r, g, b).ToString());
+                yaml.Load(r);
             }
-            sb.AppendLine();
-        }
 
-        DM = DungeonMaster.LoadEncounter(mapping, sb.ToString(), data);
-        SetupTiles();
-    }
+            var mapping = (YamlMappingNode)yaml.Documents[0].RootNode;
+            var mapFile = (mapping[new YamlScalarNode("map_file")] as YamlScalarNode).Value;
 
-    public void SetupTiles()
-    {
-        tileMap.ClearAllTiles();
-        for (int y = 0; y < DM.map.Height; y++)
-        {
-            for (int x = 0; x < DM.map.Width; x++)
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Test Map");
+            sb.AppendLine(levelLayout.width + " " + levelLayout.height);
+
+            for (int y = 0; y < levelLayout.height; y++)
             {
-                var tile = DM.map.GetTile(x, y);
-                if(tile.Passable)
+                for (int x = 0; x < levelLayout.width; x++)
                 {
-                    tileMap.SetTile(new Vector3Int(x, DM.map.Height - y, 0), emptyTile);
+                    var color = levelLayout.GetPixel(x, y);
+                    var r = (byte)(color.r * 255);
+                    var g = (byte)(color.g * 255);
+                    var b = (byte)(color.b * 255);
+                    sb.Append(data.GetIconByColor(r, g, b).ToString());
                 }
-                else
+                sb.AppendLine();
+            }
+
+            DM = DungeonMaster.LoadEncounter(mapping, sb.ToString(), data);
+            SetupTiles();
+            SetupUnits();
+        }
+
+        public void SetupTiles()
+        {
+            tileMap.ClearAllTiles();
+            for (int y = 0; y < DM.map.Height; y++)
+            {
+                for (int x = 0; x < DM.map.Width; x++)
                 {
-                    tileMap.SetTile(new Vector3Int(x, DM.map.Height - y, 0), wallTile);
+                    var tile = DM.map.GetTile(x, y);
+                    if (tile.Passable)
+                    {
+                        tileMap.SetTile(new Vector3Int(x, DM.map.Height - y, 0), emptyTile);
+                    }
+                    else
+                    {
+                        tileMap.SetTile(new Vector3Int(x, DM.map.Height - y, 0), wallTile);
+                    }
                 }
             }
         }
-    }
 
-    public void SetupUnits()
-    {
+        public void SetupUnits()
+        {
+            foreach (var guy in DM.Characters)
+            {
+                var pos = new Vector3(guy.Pos.x, DM.map.Height - guy.Pos.y, 0);
+                var go = Instantiate(characterPrefab, pos, Quaternion.identity, this.transform);
+                var controller = go.GetComponent<CharacterRenderer>();
+                controller.RepresentCharacter(guy);
+            }
+        }
 
-    }
+        // Update is called once per frame
+        void Update()
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
     }
 }
