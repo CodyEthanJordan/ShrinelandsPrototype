4242901da1a6ba05d3d5fce65c54f33d7853fe22
CodyEthanJordan <codyethanjordan@users.noreply.github.com>
2019-07-09 13:23:02 -0400

    Popup window

--- Assets/Scripts/UI/CharacterPopupUI.cs
+++ Assets/Scripts/UI/CharacterPopupUI.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
 using UnityEngine.UI;
+using ShrinelandsTactics.World;
 
 namespace Assets.Scripts.UI
 {
@@ -15,6 +16,52 @@ namespace Assets.Scripts.UI
         public Text MoveText;
         public Text StaText;
         public Text ManaText;
+        public GameObject TraitsPane;
+        public GameObject AbilityPane;
+
+        public GameObject TextPrefab;
+
+        private void Start()
+        {
+            Clear();
+        }
+
+        public void ShowCharacter(Character guy)
+        {
+            Clear(); //TODO: show side
+            NameText.text = guy.Name;
+            VitText.text = "Vit:" + guy.Vitality;
+            MoveText.text = "Move:" + guy.Move;
+            StaText.text = "Sta:" + guy.Stamina;
+            ManaText.text = "Mana:" + guy.Mana;
+            foreach (var trait in guy.Traits)
+            {
+                var text = Instantiate(TextPrefab, TraitsPane.transform);
+                text.GetComponent<Text>().text = trait;
+            }
+            foreach (var ability in guy.Actions)
+            {
+                var text = Instantiate(TextPrefab, AbilityPane.transform);
+                text.GetComponent<Text>().text = ability.Name;
+            }
+        }
+
+        public void Clear()
+        {
+            NameText.text = "";
+            VitText.text = "";
+            MoveText.text = "";
+            StaText.text = "";
+            ManaText.text = "";
+            foreach (Transform child in TraitsPane.transform)
+            {
+                Destroy(child.gameObject);
+            }
+            foreach (Transform child in AbilityPane.transform)
+            {
+                Destroy(child.gameObject);
+            }
+    }
 
     }
 }
