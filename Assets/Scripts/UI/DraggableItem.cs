59c6ad7b7dd1d3c6af04fd00fdb9e1c1e30dd7b6
CodyEthanJordan <codyethanjordan@users.noreply.github.com>
2019-07-22 02:23:41 -0400

    More stuff

---
+++ Assets/Scripts/UI/DraggableItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+namespace Assets.Scripts.UI
+{
+    public class DraggableItem : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+    {
+        public void OnBeginDrag(PointerEventData eventData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnDrag(PointerEventData eventData)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnEndDrag(PointerEventData eventData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
